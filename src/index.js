d528b82bde60960e6bb2df06cbe312b5da3cf2fb
felihong <echozhu1991@gmail.com>
2022-03-22 10:38:23 +0100

    Deprecated react routes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Configs from "./routes/configs";
-import Upload from "./routes/upload";
-import { Provider, teamsTheme, teamsV2Theme, teamsDarkTheme, teamsDarkV2Theme} from '@fluentui/react-northstar';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Provider, teamsTheme, teamsV2Theme, teamsDarkTheme, teamsDarkV2Theme} from '@fluentui/react-northstar';
 
 ReactDOM.render(
-    <Provider theme={teamsV2Theme}>
+    <Provider theme={teamsDarkTheme}>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />} />
-                {/*<Route path="configs" element={<Configs />} />*/}
-                {/*<Route path="upload" element={<Upload />} />*/}
             </Routes>
         </BrowserRouter>
     </Provider>,
