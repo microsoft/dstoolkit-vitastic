0e9eabe7cd24d5088f278ba5cdffa14c78761930
felihong <echozhu1991@gmail.com>
2022-12-09 16:39:32 +0100

    Enable external swagger API specification

--- Vitastic/api.py
+++ Vitastic/api.py
@@ -3,7 +3,7 @@ import json
 from flask import Flask, render_template, request, redirect, jsonify, url_for, make_response, send_file,  send_from_directory
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
-from flasgger import Swagger
+from flasgger import Swagger, swag_from
 from detection import detect_damage, visualize_damage
 
 
@@ -22,45 +22,17 @@ def index():
 
 
 @app.route('/services', methods=["GET"])
+@swag_from('specification/default.yaml')
 def list_service():
-    """Endpoint returning a list of supported Azure detection services
-    ---
-    responses:
-      200:
-        description: A list of supported Azure services
-    """
     result = {
         'services': ['aml', 'cv']
     }
     return jsonify(result)
 
 
-@app.route('/cv/<scope>', methods=["POST"])
-def submit_cv(scope):
-    """Endpoint submitting a job to an Azure custom vision service with specific detection scope
-    ---
-    parameters:
-      - name: scope
-        in: path
-        type: string
-        enum: ['classification', 'detection']
-        required: true
-        default: classification
-      - name: image
-        required: true
-        in: formData
-        type: file
-    definitions:
-      cv:
-        type: object
-        properties:
-          endpoint:
-            type: string
-        reference: Azure custom vision service
-    responses:
-      200:
-        description: image file with detection result
-    """
+@app.route('/cv', methods=["POST"])
+@swag_from('specification/cv.yaml')
+def submit_cv():
     f = request.files.get('image')
     filename = secure_filename(f.filename)
     f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -68,32 +40,9 @@ def submit_cv(scope):
         return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 
-@app.route('/aml/<scope>', methods=["POST"])
-def submit_aml(scope):
-    """Endpoint submitting a job to an Azure machine learning service with specific detection scope
-    ---
-    parameters:
-      - name: scope
-        in: path
-        type: string
-        enum: ['classification', 'detection', 'segmentation']
-        required: true
-        default: classification
-      - name: image
-        required: true
-        in: formData
-        type: file
-    definitions:
-      aml:
-        type: object
-        properties:
-          endpoint:
-            type: string
-        reference: Azure machine learning service
-    responses:
-      200:
-        description: base64 image encoding with detection results
-    """
+@app.route('/aml', methods=["POST"])
+@swag_from('specification/aml.yaml')
+def submit_aml():
     f = request.files.get('image')
     filename = secure_filename(f.filename)
     f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
